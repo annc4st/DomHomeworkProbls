325339c284a607b8493423ee9c8163a9d1577432
Anna C4st <annaseniuta@gmail.com>
2023-06-06 13:11:27 +0100

    just commit

---
+++ DOM_HW_6/problem6/probl6.js
@@ -0,0 +1,21 @@
+
+
+const btnBlue = document.getElementsByClassName('color').children[0];
+const btnRed = document.getElementsByClassName('color').children[1];
+
+const btnGreen = document.getElementsByTagName('color').children[2];
+const btnOrange = document.getElementsByClassName('color').children[3];
+const btnBlack = document.getElementsByClassName('color').children[4];
+
+
+function displayColor() {
+    
+}
+
+
+
+btnBlue.addEventListener("click", displayColor);
+btnRed.addEventListener("click", displayColor);
+btnGreen.addEventListener("click", displayColor);
+btnOrange.addEventListener("click", displayColor);
+btnBlack.addEventListener("click", displayColor);
